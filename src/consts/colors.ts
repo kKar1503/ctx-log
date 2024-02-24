a59422887eb7d70b6f823e4cc7dd1f7dd85a398f
Yam Kar Lok <94188697+kKar1503@users.noreply.github.com>
2024-02-24 12:15:50 +0800

    feat: Added the number for all the console output colors

---
+++ src/consts/colors.ts
@@ -0,0 +1,12 @@
+export const colorBlack = "30";
+export const colorRed = "31";
+export const colorGreen = "32";
+export const colorYellow = "33";
+export const colorBlue = "34";
+export const colorMagenta = "35";
+export const colorCyan = "36";
+export const colorWhite = "37";
+export const colorDarkGrey = "90";
+
+export const colorBold = "1";
+export const colorReset = "0";
